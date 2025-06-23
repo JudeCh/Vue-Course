deda3c17eab249676e4753c1c3e8e844a5e639d9
JudeCh <gucci0801@gmail.com>
2025-06-24 06:08:31 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <!-- App.vue預設
+   <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+
+  <MyCom />
+</template>
+
+<script>
+// App.vue預設
+// import HelloWorld from './components/HelloWorld.vue'
+// export default {
+//   name: 'App',
+//   components: {
+//     HelloWorld
+//   }
+// }
+
+import MyCom from './components/MyCom.vue'
+// 全域要export，專案版不用
+export default{
+  name:"App",
+  components:{
+    MyCom
+  }
+}
+
+
+
+</script>
+
+<style>
+/* App.vue預設 
+  #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+} */
+ 
+</style>
---
+++ src/components/MyCom.vue
@@ -0,0 +1,9 @@
+<script>
+</script>
+
+<template>
+    <h1>Welcome To Taiwan Fruit Stand</h1>
+</template>
+
+<style>
+</style>
